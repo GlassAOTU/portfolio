7598ccc2c23009dac6956940485954ea1a5e099f
Abaseen Khan <arbaseen@gmail.com>
2025-01-13 22:37:02 -0500

    animation near perfect, working on shaders now

--- main.js
+++ main.js
@@ -30,25 +30,26 @@ void main() {
     // Diagonal wind effect waves
     float windSpeed = uTime * 0.4;
     float diagonalWave = newPosition.x * 0.2 + newPosition.z * 0.7;
+    float diagonalWave2 = newPosition.x * 0.2 - newPosition.z * 0.7;
     
     // Primary diagonal waves
-    newPosition.y += sin(diagonalWave + windSpeed) * 0.3;
-    newPosition.y += cos(diagonalWave * 0.8 - windSpeed * 1.2) * 0.7;
+    // newPosition.y += sin(diagonalWave + windSpeed) * 0.3;
     
     // Secondary diagonal waves with different frequencies
     newPosition.y += sin(diagonalWave * 1.5 + windSpeed * 0.7) * 0.5;
     newPosition.y += cos(diagonalWave * 2.3 - windSpeed * 0.9) * 0.3;
+
+    newPosition.y += cos(diagonalWave2 * 0.5 - windSpeed * 0.9) * 0.3;
+    
+    // Random-looking smaller waves
+    // float noise1 = sin(newPosition.z * 2.5 + newPosition.x * 0.4 + windSpeed * 1.2) * 0.05;
+    // newPosition.y += noise1;
     
     // Add some x displacement for more natural movement
     newPosition.x += sin(diagonalWave * 0.4 + windSpeed) * 0.1;
     
-    // Random-looking smaller waves
-    float noise1 = sin(newPosition.z * 2.5 + newPosition.x * 0.4 + windSpeed * 1.2) * 0.05;
-    float noise2 = cos(newPosition.z * 3.7 - newPosition.x * 0.3 + windSpeed * 0.8) * 0.05;
-    float noise3 = sin(newPosition.z * 5.0 + newPosition.x * 0.6 + windSpeed * 1.5) * 0.05;
-    newPosition.y += noise1 + noise2 + noise3;
-    
     gl_Position = projectionMatrix * modelViewMatrix * vec4(newPosition, 1.0);
+    
 }
 `;
 // ---------------------------------- FRAGMENT SHADER
